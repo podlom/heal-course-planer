42a8229d32bb18c010cd0ccffeb776c6b3fbd26a
Shkodenko V. Taras <taras@shkodenko.com>
2019-01-24 22:09:50 +0200

    Added .gitignore, renamed course files

--- 3diahema.php
+++ 3diahema.php
@@ -1,10 +1,11 @@
 <?php
 
-define('NUM_DOSES', 28);
+define('NUM_DOSES', 60);
 
 $dt1 = new \DateTime;
-$courseStart = '29.11.2018';
+$courseStart = '24.01.2019';
 $period = '3 months';
+$boxPrice = 283.8;
 
 $courseStartDtTm = $dt1->createFromFormat('d.m.Y', $courseStart);
 echo 'Course start: ' . var_export($courseStartDtTm->format('Y-m-d'), 1) . PHP_EOL;
@@ -22,13 +23,23 @@ $daysInCourse = 0;
 foreach ($datesPeriod as $dt) {
     $daysInCourse ++;
     //
-    $dosesLeft -= 1;
-    $dosesUsed ++;
+    if ($daysInCourse == 1) {
+        $dosesPerDay = 1;
+    } else {
+        $dosesPerDay = 2;
+    }
     //
-    echo 'Day ' . $daysInCourse . ': ' . var_export($dt->format('Y-m-d'), 1) . ' box: ' . $box . ' doses used: ' . $dosesUsed . ' doses left: ' . $dosesLeft . PHP_EOL;
+    $dosesLeft -= $dosesPerDay;
+    $dosesUsed += $dosesPerDay;
     //
-    if ($dosesLeft <= 1) {
+    echo 'Day ' . $daysInCourse . ': ' . var_export($dt->format('Y-m-d'), 1) .
+        ' box: ' . $box .
+        ' doses used: ' . $dosesUsed .
+        ' doses left: ' . $dosesLeft . PHP_EOL;
+    //
+    if ($dosesLeft <= $dosesPerDay) {
         $box ++;
         $dosesLeft = NUM_DOSES;
     }
 }
+echo 'Course total price: ' . ($boxPrice * $box) . ' UAH' . PHP_EOL;
